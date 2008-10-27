3c800a00fb660878528fe715d6cfa2c351e705aa
fduraibi <fduraibi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-27 05:48:35 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-rtl/trunk@70635 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,24 @@
+=== WP-RTL ===
+Contributors: fduraibi
+Donate link:
+Tags: rtl, ltr, wysiwyg, formatting, tinymce, write, edit, post
+Requires at least: 2.6
+Tested up to: 2.6.3
+Stable tag: 0.1
+
+Adds two buttons to the TinyMCE editor to enable writing text in Left to Right (LTR) and Right to Left (RTL) directions.
+
+== Description ==
+
+This plugin enables the text directions functionality that exist already in the original TinyMCE editor which allow writing texts in Left-to-Right and Right-to-Left directions in the same post (or page).
+
+== Installation ==
+
+1. Upload the folder (wp-rtl) to the plugins directory (wp-content/plugins).
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Clear your browser cache (This is important otherwise you will see the cached TinyMCE).
+
+== Screenshots ==
+
+1. The visual TinyMCE editor showing the two added buttons and some lines in LTR and RTL directions.
+2. The HTML code with the tags to select the direction [dir="rtl" and dir="ltr"].
---
+++ wp-rtl.php
@@ -0,0 +1,49 @@
+<?php
+/*
+Plugin Name: WP-RTL
+Plugin URI: http://fadvisor.net/blog/2008/10/wp-rtl/
+Description: Adds two buttons to the TinyMCE editor to enable writing text in Left to Right (LTR) and Right to Left (RTL) directions.
+Version: 0.1
+Author: Fahad Alduraibi
+Author URI: http://fadvisor.net/blog/
+*/
+/*
+  Copyright 2008  Fahad Alduraibi  (email : fduraibi (at) gmail.com)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action( "init", "tinymce_bidi_addbuttons" );
+
+function tinymce_bidi_addbuttons() {
+	if( !current_user_can ( 'edit_posts' ) && !current_user_can ( 'edit_pages' ) ) {
+		return;
+	}
+	if( get_user_option ( 'rich_editing' ) == 'true' ) {
+		add_filter( "mce_external_plugins", "tinymce_bidi_plugin" );
+		add_filter( "mce_buttons", "tinymce_bidi_buttons" );
+	}
+}
+function tinymce_bidi_buttons($buttons) {
+	array_push($buttons, "separator", "bidi_ltr", "bidi_rtl");
+	return $buttons;
+}
+
+function tinymce_bidi_plugin($plugin_array) {
+	$plugin_array['directionality'] = WP_PLUGIN_URL.'/wp-rtl/editor_plugin.js';
+	return $plugin_array;
+}
+
+?>
